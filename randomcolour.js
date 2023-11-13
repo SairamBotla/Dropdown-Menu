dd05f74a90725f2c74210219c73469c6ef882d6d
SAIRAM BOTLA <sairambotla0416@gmail.com>
2023-11-13 14:12:25 +0530

    randomcolour.js

--- randomcolour.js
+++
@@ -1,8 +0,0 @@
- function getcolor()
-{
-    const randomno=Math.floor(Math.random()*16777215)
-    const randomcode="#"+randomno.toString(16)
-    document.body.style.backgroundColor=randomcode
-    document.getElementById('colourcode').innerText=randomcode
-  
-}
